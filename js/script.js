72ae423d93ef9bef71ddc916afebeae66aba2fa6
Pedro Henrique <seuemail@gmail.com>
2026-04-12 10:03:17 -0300

    js

--- js/script.js
+++ js/script.js
@@ -18,11 +18,19 @@ botaoTema.addEventListener('click', () => {
 });
 window.onload = function() {
     // entrada de dados
-    let idade = prompt("Bem-vindo ao recrutamento soldado! Qual a sua idade? ");
+    let nome = prompt("Qual o seu nome, soldado?")
+    let idade = prompt("Bem-vindo ao recrutamento soldado " + nome + "! Qual a sua idade? ");
+
+    // h2 html
+    const elementosaudacao = document.getElementById('mensagem-saudacao');
+
+    if (nome) {
+        elementoSaudacao.textContent ="Boa sorte na missão, soldado" + nome + "!";
+    }
 
     if (idade  >= 12) {
-        alert("Ótimo, você será enviado a Capital da Realidade Z!")
+        alert("Ótimo, você será enviado a Capital da Realidade Z!");
     } else {
-        alert("Soldado, você será enviado para a zona sul da Realidade Z")
+        alert("Soldado, você será enviado para a zona sul da Realidade Z");
     }
-}
+};
